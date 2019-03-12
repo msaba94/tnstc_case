b0bf51b876b594de4e9a44919cdf5e285e9ea792
rathina-selsoft <rathina@selsoftinc.com>
2019-03-12 10:39:33 +0530

    jasper report fixed for jar file

--- MactApplication/src/mactapplication/report/PrintReport.java
+++ MactApplication/src/mactapplication/report/PrintReport.java
@@ -5,6 +5,7 @@
  */
 package mactapplication.report;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.util.Map;
 import javax.swing.JFrame;
@@ -17,38 +18,48 @@ import net.sf.jasperreports.swing.JRViewer;
 
 /**
  *
- * @author sabapathy
+ * @author Sabapathi
  */
 public class PrintReport extends JFrame {
 
-    Connection conn;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Connection conn;
 
-    public PrintReport() {
-        conn = SQLConnection.connection();
-    }
+	public PrintReport() {
+		conn = SQLConnection.connection();
+	}
 
-    public void showReport(Map parametersMap) {
-        if (conn == null) {
-            System.out.println("Connection Failed");
-            return;
-        }
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void showReport(Map parametersMap) throws Throwable {
+		if (conn == null) {
+			System.out.println("Connection Failed");
+			return;
+		}
 
-        try {
+		String reportSource = "/mactapplication/report/CaseReport.jrxml";
+		InputStream is = null;
 
-//            JasperReport jasperReport = JasperCompileManager.compileReport("/Users/sabapathy/Sabapathi/tnstc_case/MactApplication/src/mactapplication/report/CaseReport.jrxml");
-            JasperReport jasperReport = JasperCompileManager.compileReport("/Users/sabapathy/Sabapathi/tnstc_case/MactApplication/src/mactapplication/report/CaseReport.jrxml");
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametersMap, conn);
-            JRViewer jView = new JRViewer(jasperPrint);
-            jView.setVisible(true);
-            jView.setOpaque(true);
+		try {
+			is = getClass().getResourceAsStream(reportSource);
+			// JasperReport jasperReport =
+			// JasperCompileManager.compileReport("/Users/sabapathy/Sabapathi/tnstc_case/MactApplication/src/mactapplication/report/CaseReport.jrxml");
+			JasperReport jasperReport = JasperCompileManager.compileReport(is);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametersMap, conn);
+			JRViewer jView = new JRViewer(jasperPrint);
+			jView.setVisible(true);
+			jView.setOpaque(true);
 
-            this.add(jView);
-            this.setSize(900, 500);
-            this.setVisible(true);
+			this.add(jView);
+			this.setSize(900, 500);
+			this.setVisible(true);
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			throw new Exception();
+		}
+	}
 
 }
